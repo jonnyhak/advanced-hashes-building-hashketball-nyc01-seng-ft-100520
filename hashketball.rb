b9f18deff559430b09c76642d867cf01dcfdfe13
Jonathan Hakimian <jonnyhak@gmail.com>
2020-09-21 01:10:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -64,7 +64,7 @@ def game_hash
                    :number => 4, 
                    :shoe => 18, 
                    :points => 10,
-                   :rebounds => 10,
+                   :rebounds => 1,
                    :assists => 1,
                    :steals => 2,
                    :blocks => 7,
